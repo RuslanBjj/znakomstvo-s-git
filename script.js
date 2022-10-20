1f6016e6ddd1485b6771682e122ad5ebd9ffc8ef
Ruslan <fiesta-tm@mail.ru>
2022-10-20 20:59:50 +1000

    циклы

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const listDel = prompt ("Сколько важных дел ?", "") ;
-const Zadachi = {
-Walking:{},
-study:{},
-doIt:true
-} ;
-const walk = prompt ("С кем гулял?", ""),
-    study = prompt ("Что учил?", "");
-    Zadachi.Walking[walk];
-    Zadachi.study[study];
-    console.log(Zadachi);
+const myYear = 27;
+switch (myYear) {
+    case 25: 
+    console.log('малой повзрослел');
+    break;
+    case 26: 
+    console.log('В точку');
+    break;
+    default: 
+    console.log('Возраст не важен');
+    break;
+
+}
